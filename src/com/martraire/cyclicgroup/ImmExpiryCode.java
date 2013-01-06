ccf78354551cb2366b4d808104a8769a76f4faa2
cyrille <cyrille@192.168.0.12>
2013-01-06 18:23:28 +0100

    Added partial ordering as of a reference code, still easy thanks to the underlying cyclic group of integers of order 4*10

--- src/com/martraire/cyclicgroup/ImmExpiryCode.java
+++ src/com/martraire/cyclicgroup/ImmExpiryCode.java
@@ -1,5 +1,7 @@
 package com.martraire.cyclicgroup;
 
+import java.util.Comparator;
+
 /**
  * Represents an IMM contract code in the format "U8"
  */
@@ -9,7 +11,36 @@ public class ImmExpiryCode {
 		H, M, U, Z;
 	}
 
-	private static final int ORDER = MonthCode.values().length;
+	private static final int MONTH_ORDER = MonthCode.values().length;
+	private static final int ORDER = MONTH_ORDER * 10;
+
+	/**
+	 * A comparator of ImmExpiryCode as of a given reference ImmExpiryCode
+	 */
+	public static class AsOfComparator implements Comparator<ImmExpiryCode> {
+
+		private final ImmExpiryCode ref;
+
+		public AsOfComparator(ImmExpiryCode reference) {
+			this.ref = reference;
+		}
+
+		@Override
+		public int compare(ImmExpiryCode code, ImmExpiryCode other) {
+			return normalize(code.ordinal - ref.ordinal)
+					- normalize(other.ordinal - ref.ordinal);
+		}
+
+		private int normalize(final int delta) {
+			return (delta + ORDER) % ORDER;
+		}
+
+		@Override
+		public String toString() {
+			return "ImmExpiryCode Comparator as of " + ref;
+		}
+
+	}
 
 	// the integer in the corresponding cycling group of integers
 	private final int ordinal;
@@ -21,20 +52,39 @@ public class ImmExpiryCode {
 	public static ImmExpiryCode valueOf(String code) {
 		final int year = Integer.valueOf(code.substring(code.length() - 1));
 		final MonthCode monthCode = MonthCode.valueOf(code.substring(0, 1));
-		final int ordinal = year * ORDER + monthCode.ordinal();
+		final int ordinal = year * MONTH_ORDER + monthCode.ordinal();
 		return new ImmExpiryCode(ordinal);
+	}
+
+	public ImmExpiryCode next() {
+		return roll(1);
+	}
+
+	public ImmExpiryCode previous() {
+		return roll(-1);
+	}
+
+	private ImmExpiryCode roll(int steps) {
+		return new ImmExpiryCode(ordinal + steps);
+	}
+
+	@Override
+	public int hashCode() {
+		return ordinal;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof ImmExpiryCode)) {
+			return false;
+		}
+		final ImmExpiryCode other = (ImmExpiryCode) obj;
+		return ordinal == other.ordinal;
 	}
 
 	@Override
 	public String toString() {
-		return MonthCode.values()[ordinal % ORDER].toString() + ordinal / ORDER;
-	}
-
-	public ImmExpiryCode next() {
-		return new ImmExpiryCode(ordinal + 1);
-	}
-
-	public ImmExpiryCode previous() {
-		return new ImmExpiryCode(ordinal - 1);
+		return MonthCode.values()[ordinal % MONTH_ORDER].toString() + ordinal
+				/ MONTH_ORDER;
 	}
 }
--- test/com/martraire/cyclicgroup/ImmExpiryCodeTest.java
+++ test/com/martraire/cyclicgroup/ImmExpiryCodeTest.java
@@ -3,38 +3,68 @@
  */
 package com.martraire.cyclicgroup;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.util.Comparator;
 
 import org.junit.Test;
 
+import static com.martraire.cyclicgroup.ImmExpiryCode.valueOf;
+
 public class ImmExpiryCodeTest {
 
+	private static final ImmExpiryCode H1 = valueOf("H1");
+	private static final ImmExpiryCode H2 = valueOf("H2");
+	private static final ImmExpiryCode H3 = valueOf("H3");
+	private static final ImmExpiryCode H4 = valueOf("H4");
+
 	@Test
-	public void valueOf_of_toString_is_identity() {
-		assertEquals("U8", ImmExpiryCode.valueOf("U8").toString());
-		assertEquals("U9", ImmExpiryCode.valueOf("U9").toString());
-		assertEquals("M9", ImmExpiryCode.valueOf("M9").toString());
+	public void valueOf_of_entity() {
+		assertEquals("U8", valueOf("U8").toString());
+		assertEquals("U9", valueOf("U9").toString());
+		assertEquals("M9", valueOf("M9").toString());
+		assertEquals("H9", valueOf("H9").toString());
 	}
 
 	@Test
 	public void next_code() {
-		assertEquals("Z8", ImmExpiryCode.valueOf("U8").next().toString());
-		assertEquals("Z7", ImmExpiryCode.valueOf("U7").next().toString());
+		assertEquals("Z8", valueOf("U8").next().toString());
+		assertEquals("Z7", valueOf("U7").next().toString());
 
 		// this line requires to jump to a cyclic group (40)
-		assertEquals("H8", ImmExpiryCode.valueOf("Z7").next().toString());
+		assertEquals("H8", valueOf("Z7").next().toString());
 
 		// this line would have offered to roll just the month first, leading
 		// toward another approach
-		assertEquals("U8", ImmExpiryCode.valueOf("M8").next().toString());
+		assertEquals("U8", valueOf("M8").next().toString());
 	}
 
 	@Test
-	public void previous_code() {
-		assertEquals("U8", ImmExpiryCode.valueOf("Z8").previous().toString());
-		assertEquals("U7", ImmExpiryCode.valueOf("Z7").previous().toString());
-		assertEquals("Z7", ImmExpiryCode.valueOf("H8").previous().toString());
-		assertEquals("M8", ImmExpiryCode.valueOf("U8").previous().toString());
+	public void previous_of_next_is_identity() {
+		assertEquals("Z8", valueOf("Z8").next().previous().toString());
+		assertEquals("Z7", valueOf("Z7").next().previous().toString());
+		assertEquals("H8", valueOf("H8").next().previous().toString());
+		assertEquals("U8", valueOf("U8").next().previous().toString());
 	}
 
+	@Test
+	public void actual_identity() {
+		assertEquals(valueOf("Z8"), valueOf("Z8"));
+		assertEquals(valueOf("Z7"), valueOf("Z7"));
+		assertEquals(valueOf("H8"), valueOf("H8"));
+		assertEquals(valueOf("U8"), valueOf("U8"));
+	}
+
+	@Test
+	public void ordering_is_only_partial() {
+		final Comparator<ImmExpiryCode> c = new ImmExpiryCode.AsOfComparator(H3);
+		assertEquals("ImmExpiryCode Comparator as of H3", c.toString());
+
+		assertTrue(c.compare(H3, H3) == 0);
+		assertTrue(c.compare(H3, H4) < 0);
+		assertTrue(c.compare(H4, H3) > 0);
+
+		// the tricky case as of H3
+		assertTrue(c.compare(H4, H2) < 0);
+	}
 }
